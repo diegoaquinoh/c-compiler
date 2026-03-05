8b5528997300853ec4864f94cdd63d96eec59eea
Sebastian Linares <juanselindel@gmail.com>
2026-03-05 08:32:18 +0100

    file config.example.mk and affectation v2

---
+++ compiler/config.example.mk
@@ -0,0 +1,4 @@
+ANTLRJAR=../antlr/jar/antlr-4.13.2-complete.jar
+ANTLRINC=/opt/homebrew/opt/antlr4-cpp-runtime/include/antlr4-runtime
+ANTLRLIB=/opt/homebrew/Cellar/antlr4-cpp-runtime/4.13.2/lib/libantlr4-runtime.dylib
+MACOS_DEPLOYMENT_TARGET=26.0
