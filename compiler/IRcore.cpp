c0e05438edf9334d608c2dd163421aecb9f417e2
laborylouis <165661904+laborylouis@users.noreply.github.com>
2026-03-13 10:13:30 +0100

    CFG core methods

--- compiler/IRcore.cpp
+++ compiler/IRcore.cpp
@@ -15,6 +15,58 @@ void IRInstr::gen_x86(ostream &o) {
     }
 }
 
+// CFG //
+
+void CFG::add_bb(BasicBlock* bb){
+    if (bb == nullptr) {
+        return;
+    }
+
+    bb->cfg = this;
+    this->bbs.push_back(bb);
+    this->current_bb = bb;
+}
+
+void CFG::add_to_symbol_table(string name, Type t){
+    if (this->SymbolIndex.find(name) != this->SymbolIndex.end()) {
+        return;
+    }
+
+    if (this->nextFreeSymbolIndex == 0) {
+        this->nextFreeSymbolIndex = -4;
+    }
+
+    this->nextFreeSymbolIndex -= 4;
+    this->SymbolType[name] = t;
+    this->SymbolIndex[name] = this->nextFreeSymbolIndex;
+}
+
+
+string CFG::create_new_tempvar(Type t){
+
+    int tempIndex = static_cast<int>(this->SymbolIndex.size());
+    string tempName;
+    tempName = "tmp" + to_string(tempIndex);
+
+    while (this->SymbolIndex.find(tempName) != this->SymbolIndex.end()){
+        ++tempIndex;
+        tempName = "tmp" + to_string(tempIndex);
+    }
+
+    this->add_to_symbol_table(tempName, t);
+    return tempName;
+}
+
+int CFG::get_var_index(string name){
+    return this->SymbolIndex[name];
+}
+
+Type CFG::get_var_type(string name){
+    return this->SymbolType[name];
+}
+
+
+
 
 // BasicBlock // 
 
