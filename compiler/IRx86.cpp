ae43d21324da71d05ece7d4817264bf81377505b
laborylouis <165661904+laborylouis@users.noreply.github.com>
2026-03-19 09:13:18 +0100

    all bit operators

--- compiler/IRx86.cpp
+++ compiler/IRx86.cpp
@@ -136,6 +136,7 @@ void IRInstr::gen_x86(ostream &o) {
             nameVar1 = this->params.at(0);
             index1 = this->bb->cfg->get_var_index(nameVar1);
             o <<"    negl "<< index1 <<"(%rbp)\n";
+            break;
         case IRInstr::mul:
             // var1 = var2 * var3
             nameVar1 = this->params.at(0);
@@ -167,6 +168,45 @@ void IRInstr::gen_x86(ostream &o) {
             o << "    movl " << index2 << "(%rbp), %eax" << endl;
             o << "    cltd" << endl;
             o << "    idivl " << index3 << "(%rbp)" << endl;
+            o << "    movl %eax, " << index1 << "(%rbp)" << endl;
+            break;
+        case IRInstr::bxor:
+            nameVar1 = this->params.at(0);
+            nameVar2 = this->params.at(1);
+            nameVar3 = this->params.at(2);
+
+            index1 = this->bb->cfg->get_var_index(nameVar1);
+            index2 = this->bb->cfg->get_var_index(nameVar2);
+            index3 = this->bb->cfg->get_var_index(nameVar3);
+            // Forme : var1 = var2 ^ var3
+            o << "    movl " << index2 << "(%rbp), %eax" << endl;
+            o << "    xorl " << index3 << "(%rbp), %eax" << endl;
+            o << "    movl %eax, " << index1 << "(%rbp)" << endl;
+            break;
+        case IRInstr::bor:
+            nameVar1 = this->params.at(0);
+            nameVar2 = this->params.at(1);
+            nameVar3 = this->params.at(2);
+
+            index1 = this->bb->cfg->get_var_index(nameVar1);
+            index2 = this->bb->cfg->get_var_index(nameVar2);
+            index3 = this->bb->cfg->get_var_index(nameVar3);
+            // Forme : var1 = var2 | var3
+            o << "    movl " << index2 << "(%rbp), %eax" << endl;
+            o << "    orl " << index3 << "(%rbp), %eax" << endl;
+            o << "    movl %eax, " << index1 << "(%rbp)" << endl;
+            break;
+        case IRInstr::band:
+            nameVar1 = this->params.at(0);
+            nameVar2 = this->params.at(1);
+            nameVar3 = this->params.at(2);
+
+            index1 = this->bb->cfg->get_var_index(nameVar1);
+            index2 = this->bb->cfg->get_var_index(nameVar2);
+            index3 = this->bb->cfg->get_var_index(nameVar3);
+            // Forme : var1 = var2 & var3
+            o << "    movl " << index2 << "(%rbp), %eax" << endl;
+            o << "    andl " << index3 << "(%rbp), %eax" << endl;
             o << "    movl %eax, " << index1 << "(%rbp)" << endl;
             break;
         default:
