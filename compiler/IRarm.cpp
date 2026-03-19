d373185dd1ec15abc2ea1dc8fb6155320adab63a
Erwann HEQUET <hequet.erwann@gmail.com>
2026-03-19 11:58:00 +0100

    traduction vers ARM

--- compiler/IRarm.cpp
+++ compiler/IRarm.cpp
@@ -184,6 +184,188 @@ void IRInstr::gen_arm(ostream &o) {
             o << "    sdiv w0, w8, w9\n";
             o << "    str w0, [sp, #" << index1 << "]\n";
             break;
+        case IRInstr::mod:
+            // Forme : var1 = var2 % var3
+            nameVar1 = this->params.at(0);
+            nameVar2 = this->params.at(1);
+            nameVar3 = this->params.at(2);
+
+            this->bb->cfg->add_to_symbol_table(nameVar1, this->t);
+
+            index1 = this->bb->cfg->get_var_index_arm(nameVar1);
+            index2 = this->bb->cfg->get_var_index_arm(nameVar2);
+            index3 = this->bb->cfg->get_var_index_arm(nameVar3);
+
+            // On utilise le fait que : (c'est ce que fait gcc)
+            // n = p * q +r avec :
+            // a = p // q (div entière)
+            // b = a * q (= n - r)
+            // r = p - b
+            o << "    ldr w8, [sp, #" << index2 << "]" << endl;
+            o << "    ldr w9, [sp, #" << index3 << "]" << endl;
+            o << "    sdiv w10, w8, w9" << endl;
+            o << "    mul w10, w10, w9" << endl;
+            o << "    sub w0, w8, w10" << endl;
+            o << "    str w0, [sp, #" << index1 << "]" << endl;
+
+            break;
+        case IRInstr::lnot:
+            // Forme : var1 = !var2
+            nameVar1 = this->params.at(0);
+            nameVar2 = this->params.at(1);
+
+            this->bb->cfg->add_to_symbol_table(nameVar1, this->t);
+
+            index1 = this->bb->cfg->get_var_index_arm(nameVar1);
+            index2 = this->bb->cfg->get_var_index_arm(nameVar2);
+
+            // On fait comme gcc, on utilise le fait que subs va positionner le flag Z si le résultat est nul 
+            // eq est un test pour savoir si z == 1
+            o << "    ldr w8, [sp, #" << index2 << "]" << endl;
+            o << "    subs w8, w8, #0" << endl;
+            o << "    cset w0, eq" << endl;
+            o << "    str w0, [sp, #" << index1 << "]" << endl;
+
+            break;
+        case IRInstr::bxor:
+            nameVar1 = this->params.at(0);
+            nameVar2 = this->params.at(1);
+            nameVar3 = this->params.at(2);
+
+            index1 = this->bb->cfg->get_var_index_arm(nameVar1);
+            index2 = this->bb->cfg->get_var_index_arm(nameVar2);
+            index3 = this->bb->cfg->get_var_index_arm(nameVar3);
+            // Forme : var1 = var2 ^ var3
+            o << "    ldr w8, [sp, #" << index2 << "]\n";
+            o << "    ldr w9, [sp, #" << index3 << "]\n";
+            o << "    eor w0, w8, w9\n";
+            o << "    str w0, [sp, #" << index1 << "]\n";
+            break;
+        case IRInstr::bor:
+            nameVar1 = this->params.at(0);
+            nameVar2 = this->params.at(1);
+            nameVar3 = this->params.at(2);
+
+            index1 = this->bb->cfg->get_var_index_arm(nameVar1);
+            index2 = this->bb->cfg->get_var_index_arm(nameVar2);
+            index3 = this->bb->cfg->get_var_index_arm(nameVar3);
+            // Forme : var1 = var2 | var3
+            o << "    ldr w8, [sp, #" << index2 << "]\n";
+            o << "    ldr w9, [sp, #" << index3 << "]\n";
+            o << "    orr w0, w8, w9\n";
+            o << "    str w0, [sp, #" << index1 << "]\n";
+            break;
+        case IRInstr::band:
+            nameVar1 = this->params.at(0);
+            nameVar2 = this->params.at(1);
+            nameVar3 = this->params.at(2);
+
+            index1 = this->bb->cfg->get_var_index_arm(nameVar1);
+            index2 = this->bb->cfg->get_var_index_arm(nameVar2);
+            index3 = this->bb->cfg->get_var_index_arm(nameVar3);
+            // Forme : var1 = var2 & var3
+            o << "    ldr w8, [sp, #" << index2 << "]\n";
+            o << "    ldr w9, [sp, #" << index3 << "]\n";
+            o << "    and w0, w8, w9\n";
+            o << "    str w0, [sp, #" << index1 << "]\n";
+            break;
+        case IRInstr::cmp_eq:
+            nameVar1 = this->params.at(0);
+            nameVar2 = this->params.at(1);
+            nameVar3 = this->params.at(2);
+
+            index1 = this->bb->cfg->get_var_index_arm(nameVar1);
+            index2 = this->bb->cfg->get_var_index_arm(nameVar2);
+            index3 = this->bb->cfg->get_var_index_arm(nameVar3);
+            // Forme : var1 = var2 == var3
+            // var2 == var3 <=> var2 - var3 == 0
+            // subs va positionner le flag en fonction du résultat
+            o << "    ldr w8, [sp, #" << index2 << "]" << endl;
+            o << "    ldr w9, [sp, #" << index3 << "]\n";
+            o << "    subs w8, w8, w9" << endl;
+            o << "    cset w0, eq" << endl;
+            o << "    str w0, [sp, #" << index1 << "]" << endl;
+            break;
+        case IRInstr::cmp_ne:
+            nameVar1 = this->params.at(0);
+            nameVar2 = this->params.at(1);
+            nameVar3 = this->params.at(2);
+
+            index1 = this->bb->cfg->get_var_index_arm(nameVar1);
+            index2 = this->bb->cfg->get_var_index_arm(nameVar2);
+            index3 = this->bb->cfg->get_var_index_arm(nameVar3);
+            // var1 = var2 != var3
+            // Même fonctionnement que pour eq mais on regardre si z == 0
+            o << "    ldr w8, [sp, #" << index2 << "]" << endl;
+            o << "    ldr w9, [sp, #" << index3 << "]\n";
+            o << "    subs w8, w8, w9" << endl;
+            o << "    cset w0, ne" << endl;
+            o << "    str w0, [sp, #" << index1 << "]" << endl;
+            break;
+        case IRInstr::cmp_lt:
+            nameVar1 = this->params.at(0);
+            nameVar2 = this->params.at(1);
+            nameVar3 = this->params.at(2);
+
+            index1 = this->bb->cfg->get_var_index_arm(nameVar1);
+            index2 = this->bb->cfg->get_var_index_arm(nameVar2);
+            index3 = this->bb->cfg->get_var_index_arm(nameVar3);
+
+            //var1 = var2 < var3
+            o << "    ldr w8, [sp, #" << index2 << "]" << endl;
+            o << "    ldr w9, [sp, #" << index3 << "]\n";
+            o << "    subs w8, w8, w9" << endl;
+            o << "    cset w0, lt" << endl;
+            o << "    str w0, [sp, #" << index1 << "]" << endl;
+            break;
+        case IRInstr::cmp_le:
+            nameVar1 = this->params.at(0);
+            nameVar2 = this->params.at(1);
+            nameVar3 = this->params.at(2);
+
+            index1 = this->bb->cfg->get_var_index_arm(nameVar1);
+            index2 = this->bb->cfg->get_var_index_arm(nameVar2);
+            index3 = this->bb->cfg->get_var_index_arm(nameVar3);
+
+            //var1 = var2 <= var3
+            o << "    ldr w8, [sp, #" << index2 << "]" << endl;
+            o << "    ldr w9, [sp, #" << index3 << "]\n";
+            o << "    subs w8, w8, w9" << endl;
+            o << "    cset w0, le" << endl;
+            o << "    str w0, [sp, #" << index1 << "]" << endl;
+            break;
+        case IRInstr::cmp_gt:
+            nameVar1 = this->params.at(0);
+            nameVar2 = this->params.at(1);
+            nameVar3 = this->params.at(2);
+
+            index1 = this->bb->cfg->get_var_index_arm(nameVar1);
+            index2 = this->bb->cfg->get_var_index_arm(nameVar2);
+            index3 = this->bb->cfg->get_var_index_arm(nameVar3);
+
+            //var1 = var2 > var3
+            o << "    ldr w8, [sp, #" << index2 << "]" << endl;
+            o << "    ldr w9, [sp, #" << index3 << "]\n";
+            o << "    subs w8, w8, w9" << endl;
+            o << "    cset w0, gt" << endl;
+            o << "    str w0, [sp, #" << index1 << "]" << endl;
+            break;
+        case IRInstr::cmp_ge:
+            nameVar1 = this->params.at(0);
+            nameVar2 = this->params.at(1);
+            nameVar3 = this->params.at(2);
+
+            index1 = this->bb->cfg->get_var_index_arm(nameVar1);
+            index2 = this->bb->cfg->get_var_index_arm(nameVar2);
+            index3 = this->bb->cfg->get_var_index_arm(nameVar3);
+
+            //var1 = var2 >= var3
+            o << "    ldr w8, [sp, #" << index2 << "]" << endl;
+            o << "    ldr w9, [sp, #" << index3 << "]\n";
+            o << "    subs w8, w8, w9" << endl;
+            o << "    cset w0, ge" << endl;
+            o << "    str w0, [sp, #" << index1 << "]" << endl;
+            break;
         default:
             break;
     }
