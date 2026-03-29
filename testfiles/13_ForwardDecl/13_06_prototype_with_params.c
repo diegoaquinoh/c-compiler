f57d55a0dbde35c15c6af5f4e8cdb4ffcce5bd8f
diegoaquinoh <diegoaquinoh@gmail.com>
2026-03-29 13:40:01 +0200

    add : tests for forward declaration

---
+++ testfiles/13_ForwardDecl/13_06_prototype_with_params.c
@@ -0,0 +1,9 @@
+int compute(int a, int b, int c);
+
+int main() {
+    return compute(1, 2, 3);
+}
+
+int compute(int a, int b, int c) {
+    return a + b + c;
+}
---
+++ testfiles/13_ForwardDecl/13_10_conflicting_decl_after_def.c
@@ -0,0 +1,9 @@
+int foo(int x) {
+    return x;
+}
+
+int foo(int x, int y);
+
+int main() {
+    return 0;
+}
